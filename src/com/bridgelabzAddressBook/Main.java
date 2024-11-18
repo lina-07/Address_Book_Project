4a58601697f7860d7bc46d1b9a40a4360793b9c5
lina <linabirari74@gmail.com>
2024-11-19 03:28:32 +0530

    Added to Master

--- src/com/bridgelabzAddressBook/Main.java
+++ src/com/bridgelabzAddressBook/Main.java
@@ -1,8 +1,7 @@
+package com.bridgelabzAddressBook;
 
-public class AddressBookMain {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book Management System ");
-
-
-        }
     }
+}

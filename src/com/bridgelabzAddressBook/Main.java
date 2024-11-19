2710c64c142427ca148258ac5c5c2912d23f5e16
lina <linabirari74@gmail.com>
2024-11-19 07:33:22 +0530

    Welcome to Address Book Program

--- src/com/bridgelabzAddressBook/Main.java
+++ src/com/bridgelabzAddressBook/Main.java
@@ -1,7 +1,9 @@
 package com.bridgelabzAddressBook;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Management System ");
+        System.out.println("Welcome to the Address Book ");
+
     }
 }

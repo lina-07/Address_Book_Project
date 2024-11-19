8e8c6a9c4720847b1d38107da0e265e4e6dc4a39
lina <linabirari74@gmail.com>
2024-11-20 00:50:52 +0530

    Added: UC2 Ability to add a new contact

---
+++ src/com/bridgelabzAddressBook/AddressBook.java
@@ -0,0 +1,13 @@
+package com.bridgelabzAddressBook;
+
+import java.util.ArrayList;
+
+public class AddressBook
+{
+    public ArrayList<Details> contactList = new ArrayList<>();
+    public  ArrayList<Details> contactList(Details d)
+    {
+        contactList.add(d);
+        return contactList;
+    }
+}
--- src/com/bridgelabzAddressBook/AddressBookMain.java
+++ src/com/bridgelabzAddressBook/AddressBookMain.java
@@ -1,9 +1,42 @@
 package com.bridgelabzAddressBook;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class AddressBookMain {
+public class AddressBookMain
+{
+    public static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Welcome to the Address Book ");
+
+        AddressBook addressBook = new AddressBook();
+
+        System.out.println("Enter the contact details.............");
+        System.out.println("Enter the first name:");
+        String firstName = sc.next();
+        System.out.println("Enter the last name:");
+        String lastName = sc.next();
+        System.out.println("Enter the address:");
+        String address = sc.next();
+        System.out.println("Enter the city:");
+        String city = sc.next();
+        System.out.println("Enter the state:");
+        String state = sc.next();
+        System.out.println("Enter the zip code:");
+        String zip = sc.next();
+        System.out.println("Enter the phone no:");
+        String phoneNumber = sc.next();
+        System.out.println("Enter the email:");
+        String email = sc.next();
+        Details d = new Details(firstName, lastName, address, city, state, email, phoneNumber, zip);
+
+        ArrayList<Details> contactList = addressBook.contactList(d);
+        for(Details contact:contactList)
+        {
+            System.out.println(contact.getFirstName()+" "+contact.getLastName()+" "+contact.getAddress()+" "+contact.getCity()+
+                    " "+contact.getState()+" "+contact.getEmail()+" "+contact.getPhoneNumber()+" "+contact.getZip());
+        }
 
     }
 }
